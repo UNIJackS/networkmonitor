3b84a3454ca48d1efdf3a3d988effb89d155f4d6
Jack Scrivener <10jack222@gmail.com>
2024-10-30 01:42:39 +1300

    Commented

--- src/main/java/com/github/unijacks/DeviceManager.java
+++ src/main/java/com/github/unijacks/DeviceManager.java
@@ -12,42 +12,45 @@ import javafx.scene.layout.FlowPane;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import java.util.Collections;
+/*
+ * Manages a set of devices.
+ */
 public class DeviceManager {
-    private Queue<Device> devices;
+    private Queue<Device> devicesQueue;
     private Map<String, Device> devicesMap;
 
+    /*
+     * Loads all the devices and provices a way to manage them
+     */
     public DeviceManager() {
         System.out.println("Loading devices ...");
-        // Intalises the priority queue
-        devices = new PriorityQueue<>();
-        // Loads all the devices into the priority queue from file
-        loadFromFile();
-        // Loads all the devices from the priority queue into the map
+        // Intalises the priority queue and hashmap.
+        devicesQueue = new PriorityQueue<>();
         devicesMap = new HashMap<>();
-
+        // Loads devices from Devices.txt.
+        loadFromFile();
         System.out.println("Devices loaded sucessfully");
     }
 
-    public Map<String, Device> getDevicesMap() {
-        if (devicesMap.size() == 0) {
-            Queue<Device> devicesCopy = new PriorityQueue<>(devices);
-            while (!devicesCopy.isEmpty()) {
-                Device currentDevice = devicesCopy.poll();
-                devicesMap.put(currentDevice.getStrIP(), currentDevice);
-            }
-        }
-        return devicesMap;
-    }
+    //Returns a unmodifiable map of the devices.
+    public Map<String, Device> getDevicesMap() {return Collections.unmodifiableMap(devicesMap);}
 
+    /*
+     * Takes a flow plane and updates its children to contain the cards of all the devices.
+     */
     public void updateFlowPane(FlowPane flowPlaneInput) {
         flowPlaneInput.getChildren().clear();
-        Queue<Device> devicesCopy = new PriorityQueue<>(devices);
+        Queue<Device> devicesCopy = new PriorityQueue<>(devicesQueue);
         while (!devicesCopy.isEmpty()) {
             Device currentDevice = devicesCopy.poll();
             flowPlaneInput.getChildren().add(currentDevice.getCard());
         }
     }
 
+    /*
+     * Creates a scanner of a file at a given path.
+     */
     public static Scanner readFile(String pathName) {
         try {
             File myObj = new File(pathName);
@@ -61,33 +64,41 @@ public class DeviceManager {
     }
 
     // Loads the devices from the DeviceList.txt file.
-    public void loadFromFile() {
+    private void loadFromFile() {
         // Clears the devices set.
-        devices.clear();
+        devicesQueue.clear();
         // Creates a scanner from the DeviceList.txt file
         Scanner linesInFileScanner = readFile("DeviceList.txt");
         // Loops through the lines in the file.
         while (linesInFileScanner.hasNextLine()) {
-            String line = linesInFileScanner.nextLine(); // Gets the nextline from the DeviceList.txt file scanner
-            devices.add(new Device(new Scanner(line)));
+            // Creates a new device from a scanner of the line in the file.
+            Device newDevice = new Device(new Scanner(linesInFileScanner.nextLine()));
+            devicesQueue.add(newDevice);
+            devicesMap.put(newDevice.getStrIP(),newDevice);
         }
         linesInFileScanner.close();
     }
 
+    /*
+     * Prints all the devices currently loaded to the terminal.
+     */
     public void printAll() {
-        Queue<Device> devicesCopy = new PriorityQueue<>(devices);
+        Queue<Device> devicesCopy = new PriorityQueue<>(devicesQueue);
         while (!devicesCopy.isEmpty()) {
             System.out.println(devicesCopy.poll());
         }
     }
 
+    /*
+     * Pings all the devices currently loaded.
+     */
     public void pingAll() {
-        Queue<Device> devicesCopy = new PriorityQueue<>(devices);
+        Queue<Device> devicesCopy = new PriorityQueue<>(devicesQueue);
         while (!devicesCopy.isEmpty()) {
             Device currentDevice = devicesCopy.poll();
-            if(currentDevice.ping(false)){
-                break;
-            }
+            // Ping only returns true when it actually attempts to ping a device.
+            // The loop is broken when we actually attempt ping a device to space out the device pings.
+            if(currentDevice.ping(false)){break;}
         }
     }
 
